6646cd95d52f79e404054d144e8aabbfe3deed0e
varsha2176 <aaruna.varshhh0760@gmail.com>
2025-06-13 13:17:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/score/:username', async (req, res) => {
+  const username = req.params.username;
+  try {
+    const { data } = await axios.get(`https://api.github.com/users/${username}`);
+    const score = data.public_repos * 2 + data.followers * 3;
+    res.json({ username, score });
+  } catch (error) {
+    res.status(404).json({ error: 'User not found' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
